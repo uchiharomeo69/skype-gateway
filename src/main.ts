69cf19a525c6a8d2eac37d5e9b73e265f56e3508
Your Name <uchiharomeo69@gmail.com>
2021-10-27 16:18:57 +0700

    feat:add all

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
